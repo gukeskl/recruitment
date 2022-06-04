a6dc1c6ec9ecb118eaa33259d66e0fa96b8d5ddc
MysteriousNut <jm.markowski@outlook.com>
2022-06-04 19:52:02 +0200

    create card component

---
+++ src/components/card/index.tsx
@@ -0,0 +1,39 @@
+import { AiFillHeart } from 'react-icons/ai';
+
+type CardProps = {
+  title: string;
+  imageUrl: string;
+  onClick?: () => void;
+  isLikeable?: boolean;
+  isLiked?: boolean;
+  onLike?: () => void;
+};
+
+export const Card: React.FC<CardProps> = ({ title, imageUrl, isLikeable, isLiked, onClick, onLike = () => null }) => (
+  <div onClick={onClick} className='card is-clickable' style={{ position: 'relative' }}>
+    <div className='card-image'>
+      <figure className='image is-4by3'>
+        <img src={imageUrl} alt='Placeholder image' />
+      </figure>
+    </div>
+    <div className='card-content'>
+      <div className='media'>
+        <div className='media-content'>
+          <p className='title is-5'>{title}</p>
+        </div>
+      </div>
+    </div>
+    {isLikeable && (
+      <button
+        className='button m-2 p-2'
+        style={{ position: 'absolute', right: '0px', top: '0px' }}
+        onClick={e => {
+          e.stopPropagation();
+          onLike();
+        }}
+      >
+        <AiFillHeart color={isLiked ? 'pink' : 'grey'} size={30} />
+      </button>
+    )}
+  </div>
+);
