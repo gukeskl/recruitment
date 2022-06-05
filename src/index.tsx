327df6a6a314a9f7be63f58031498c51609e39da
MysteriousNut <jm.markowski@outlook.com>
2022-06-05 21:51:35 +0200

    remove react strict mode

--- src/index.tsx
+++ src/index.tsx
@@ -9,9 +9,7 @@ import 'bulma/css/bulma.css';
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
 
 root.render(
-  <React.StrictMode>
-    <Provider store={store}>
-      <Router />
-    </Provider>
-  </React.StrictMode>
+  <Provider store={store}>
+    <Router />
+  </Provider>
 );
