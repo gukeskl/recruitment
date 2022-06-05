582f25dbd9fa8ec45c7122ad661376163f4a8ea7
MysteriousNut <jm.markowski@outlook.com>
2022-06-05 09:05:54 +0200

    create basics routes layouts

--- src/router/base/index.tsx
+++ src/router/base/index.tsx
@@ -1,5 +1,25 @@
+import { useNavigate } from 'react-router-dom';
+import { Card } from 'components';
+
 const BasePage: React.FC = () => {
-  return <>Base</>;
+  const navigate = useNavigate();
+
+  return (
+    <div className='container p-4'>
+      <div className='level is-flex is-justify-content-right'>
+        <button className='button'>FAVORITES</button>
+      </div>
+      <div className='columns is-multiline is-centered'>
+        <div className='column is-one-third is-one-quarter-fullhd'>
+          <Card
+            title='name'
+            imageUrl='https://images.dog.ceo/breeds/corgi-cardigan/n02113186_9615.jpg'
+            onClick={() => navigate('/name')}
+          />
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default BasePage;
--- src/router/breed/index.tsx
+++ src/router/breed/index.tsx
@@ -1,9 +1,24 @@
 import { useParams } from 'react-router-dom';
+import { Card } from 'components';
 
 const BreedPage: React.FC = () => {
   const { breed } = useParams();
 
-  return <>{breed}</>;
+  return (
+    <div className='container p-4'>
+      <div className='columns is-multiline is-centered'>
+        <div className='column is-one-third is-one-quarter-fullhd'>
+          <Card
+            title={breed || 'ups'}
+            imageUrl='https://images.dog.ceo/breeds/corgi-cardigan/n02113186_9615.jpg'
+            isLikeable={true}
+            isLiked={true}
+            onLike={() => console.log('like')}
+          />
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default BreedPage;
