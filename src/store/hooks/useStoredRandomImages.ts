9fc9efa8498edfbba9ed7e1e36652a8ec688d788
MysteriousNut <jm.markowski@outlook.com>
2022-06-14 20:25:30 +0200

    create method in useStoredRandomImages hook to fetch random images when it is needed, instead of all at the beginning

--- src/store/hooks/useStoredRandomImages.ts
+++ src/store/hooks/useStoredRandomImages.ts
@@ -1,24 +1,29 @@
 import breedsApi from 'breeds-api';
-import { useEffect } from 'react';
+import { useCallback } from 'react';
 import { useDispatch } from 'react-redux';
 import { useAppSelector } from 'store';
 import { setRandomImageUrl } from 'store/slices/breeds';
+import { Breed } from 'types';
 
 export const useStoredRandomImages = () => {
   const dispatch = useDispatch();
-  const { breeds, randomImagesUrls } = useAppSelector(store => ({ ...store.breeds }));
+  const { randomImagesUrls } = useAppSelector(store => ({ ...store.breeds }));
 
-  useEffect(() => {
-    breeds?.forEach(async breed => {
-      if (!randomImagesUrls[breed]) {
-        const url = await breedsApi.getRandomDogImage(breed);
-        dispatch(setRandomImageUrl({ breed, url }));
-      }
-    });
+  const fetchRandomImagesUrls = useCallback(
+    async (breeds: Breed[]) => {
+      breeds?.forEach(async breed => {
+        if (!randomImagesUrls[breed]) {
+          const url = await breedsApi.getRandomDogImage(breed);
+          dispatch(setRandomImageUrl({ breed, url }));
+        }
+      });
+    },
     // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, [breeds, dispatch]);
+    [dispatch]
+  );
 
   return {
     randomImagesUrls,
+    fetchRandomImagesUrls,
   };
 };
